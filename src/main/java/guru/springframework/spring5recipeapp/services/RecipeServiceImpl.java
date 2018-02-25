8e9950f9366bf2471bb3e38eda7b8eea0c3164a5
jperezdelafuente <jesusperez82@gmail.com>
2018-02-25 13:55:08 +0100

    adding Project Lombok with @Slf4j in Controller, Service and Bootstrap class

--- src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package guru.springframework.spring5recipeapp.services;
 
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I´m in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         this.recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
